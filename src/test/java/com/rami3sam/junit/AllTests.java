b4e9851b95e99d5b5807a1c88bdd0ced832894ec
Rami Essamedeen <rami3sam@gmail.com>
2022-05-23 15:55:06 -0400

    wrote test for getPageLinks function

--- src/test/java/com/rami3sam/junit/AllTests.java
+++ src/test/java/com/rami3sam/junit/AllTests.java
@@ -1,20 +1,19 @@
 package com.rami3sam.junit;
 
 import com.rami3sam.crawler.Util;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.BeforeEach;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class AllTests {
 
     @Test
-    void test(){
+    void testProcessURLFunction(){
 
         URI URI1 = null;
         URI URI2 = null;
@@ -45,16 +44,30 @@ public class AllTests {
         testCases.add(new URLTest("https://google.com/a/b/c/d", URI2,"d"));
 
         for (URLTest testCase : testCases){
-            assertEquals(testCase.expected,Util.processURL(testCase.pageURI,testCase.URL));
+            assertEquals(testCase.expected,Util.processURL(testCase.pageURI,testCase.URL),"Failed on case: "+testCase.expected);
         }
 
     }
+    @Test
+    void testGetPageLinksFunction(){
+        ArrayList<String> links = Util.getPageLinks("https://monzo.com");
 
+        String[] expectedLinks = {"https://monzo.com",
+                "https://monzo.com/features/energy-switching",
+                "https://monzo.com/i/savingwithmonzo",
+                "https://monzo.com/isa",
+                "https://monzo.com/i/monzo-premium"
+        };
+
+        for (String expectedLink : expectedLinks){
+            assertTrue(links.contains(expectedLink) ,"Failed on link: "+expectedLink);
+        }
+
+    }
     private class URLTest{
         String expected;
         String URL;
         URI pageURI;
-
         public URLTest(String expected,URI pageURI, String URL) {
             this.expected = expected;
             this.URL = URL;
